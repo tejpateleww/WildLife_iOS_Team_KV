044daf89c999b1917c988959a8d0c9a971b4ecb5
Satyajit <satyajit@excellentwebworld.in>
2020-07-06 11:30:39 +0530

    Implement Core Data Stack for Map Dealers , Lists and Init API.

---
+++ Wildlife Research Center/Model/MapDealerData.swift
@@ -0,0 +1,9 @@
+//
+//  MapDealerData.swift
+//  Wildlife Research Center
+//
+//  Created by EWW074 - Sj's iMAC on 02/07/20.
+//  Copyright © 2020 EWW074. All rights reserved.
+//
+
+import Foundation
---
+++ Wildlife Research Center/SupportingFiles/Database/DatabaseHandler.swift
@@ -0,0 +1,9 @@
+//
+//  DatabaseHandler.swift
+//  Wildlife Research Center
+//
+//  Created by EWW074 - Sj's iMAC on 01/07/20.
+//  Copyright © 2020 EWW074. All rights reserved.
+//
+
+import Foundation
